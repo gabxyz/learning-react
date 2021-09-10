c097bcd5b632ffcf96450b2ad74221c7da72cead
Gabriel Rodrigues <gabrxzvski@gmail.com>
2021-09-10 10:55:09 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EventBind />
+      {/* <ClassClick />
+      <FunctionClick />
+      <Counter />
+      <Message />
+      <Greet name="Gabriel" heroName="Spider-man">
+       <p>This is children props</p> 
+      </Greet>
+      <Greet name="Luana" heroName="Wonder Woman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Irineu" heroName="Batman" />
+      <Welcome name="Gabriel" heroName="Spider-man" />
+      <Welcome name="Luana" heroName="Wonder Woman" />
+      <Welcome name="Irineu" heroName="Batman" />
+      <Hello /> */}
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React, { createElement } from "react";
+
+const Hello = () => {
+  return (
+    <div className="dummyClass">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default Hello;
